90d80fa1c9b892fd22f41091bbe45659c1ea88c5
Prashant Aggarwal <prashant.aggarwal.2@alight.com>
2022-01-27 19:28:50 +0530

    Practice Updates

--- src/main/java/com/pc/data/StudentDataBase.java
+++ src/main/java/com/pc/data/StudentDataBase.java
@@ -46,4 +46,8 @@ public class StudentDataBase {
         List<Student> students = Arrays.asList(student1,student2,student3,student4,student5,student6);
         return students;
     }
+    
+    public static Boolean greaterThanGradeLevel(Student student) {
+    	return student.getGradeLevel()>=2;
+    }
 }
